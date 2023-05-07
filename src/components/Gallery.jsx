eeba88a23afb243e956f7f1e5134726b34b70954
mardiansyah <mardiansyahm002@gmail.com>
2023-05-07 11:59:13 +0700

    update

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function Gallery() {
   return (
     <>
-      <h1 className="text-center font-bold py-10 text-3xl dark:text-white">Galeri kami</h1>
+      <h1 className="text-center font-bold py-10 text-3xl ">Galeri kami</h1>
       <div className="container lg:px-32 px-4 py-8 mx-auto items-center ">
         <div className="grid grid-cols-4 gap-3">
           <Image
