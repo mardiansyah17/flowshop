c461706aad251e162837a4a6e0b95283afe17e83
mardiansyah17 <mardiansyahm002@gmail.com>
2023-09-13 11:03:47 +0700

    update

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl:  'http://flowshop.mardiancode.tech/',
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+}
